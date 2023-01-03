67ff35faa41b5fce4f69928a4c99886fcd3a7fbe
z1xonu4a <hedhet68@gmail.com>
2023-01-03 08:36:00 +0300

    add prettier and eslint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ee5677240fddc07"; }
+/******/ 		__webpack_require__.h = function() { return "f452ee9aa2b9965b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
