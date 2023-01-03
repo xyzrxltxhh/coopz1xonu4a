fc178d85ef6e3b16703d09455907b429a745d844
z1xonu4a <hedhet68@gmail.com>
2023-01-03 09:12:22 +0300

    add articles for test

---
+++ components/articles.js
@@ -0,0 +1,57 @@
+import { Container } from '@chakra-ui/react'
+
+const Articles = () => {
+	return (
+		<Container pt={'20'}>
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+			jaladijf;lsdakjf;lskdjfldsakjf;sdlkjfs;dlkjfsda;lkjfsda;lkfjsd;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsda;lfkjsad;lkfjsad;lkfjsdal;kfjsad;lfkjsda;lfkjsda;lfkjsda;lfkjsadlkj
+		</Container>
+	)
+}
+
+export default Articles
