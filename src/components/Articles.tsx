7622963b948618ca0b4ee3eb8b477338e8b0a6e2
z1xonu4a <hedhet68@gmail.com>
2023-01-07 06:03:34 +0300

    Add Articles file

---
+++ src/components/Articles.tsx
@@ -0,0 +1,50 @@
+import { Container } from '@chakra-ui/react'
+
+export const Articles = () => {
+	return (
+		<Container textAlign={'center'} justifyContent={'center'} pt={'24'}>
+			Skip to content Search or jump to… Pull requests Issues Codespaces
+			Marketplace Explore @Z1xonu4a Directory successfully deleted. xyzrxltxhh /
+			coopz1xonu4a Public Code Issues Pull requests Actions Projects Wiki
+			Security Insights xyzrxltxhh/coopz1xonu4a 2 branches 0 tags Latest commit
+			@Z1xonu4a Z1xonu4a Delete .next directory 818b8ef 25 minutes ago Git stats
+			25 commits Files Type Name Latest commit message Commit time public add 38
+			minutes ago src add 38 minutes ago .gitignore Add index.js and _app.js,
+			and npm next+chakra-ui 4 days ago .prettierrc Add index.js and _app.js,
+			and npm next+chakra-ui 4 days ago README.md add 38 minutes ago
+			package-lock.json add 38 minutes ago package.json add 38 minutes ago
+			tsconfig.json add 38 minutes ago README.md This project was bootstrapped
+			with Create React App. Available Scripts In the project directory, you can
+			run: npm start Runs the app in the development mode. Open
+			http://localhost:3000 to view it in the browser. The page will reload if
+			you make edits. You will also see any lint errors in the console. npm test
+			Launches the test runner in the interactive watch mode. See the section
+			about running tests for more information. npm run build Builds the app for
+			production to the build folder. It correctly bundles React in production
+			mode and optimizes the build for the best performance. The build is
+			minified and the filenames include the hashes. Your app is ready to be
+			deployed! See the section about deployment for more information. npm run
+			eject Note: this is a one-way operation. Once you eject, you can’t go
+			back! If you aren’t satisfied with the build tool and configuration
+			choices, you can eject at any time. This command will remove the single
+			build dependency from your project. Instead, it will copy all the
+			configuration files and the transitive dependencies (webpack, Babel,
+			ESLint, etc) right into your project so you have full control over them.
+			All of the commands except eject will still work, but they will point to
+			the copied scripts so you can tweak them. At this point you’re on your
+			own. You don’t have to ever use eject. The curated feature set is suitable
+			for small and middle deployments, and you shouldn’t feel obligated to use
+			this feature. However we understand that this tool wouldn’t be useful if
+			you couldn’t customize it when you are ready for it. Learn More You can
+			learn more in the Create React App documentation. To learn React, check
+			out the React documentation. About No description, website, or topics
+			provided. Resources Readme Stars 0 stars Watchers 1 watching Forks 0 forks
+			Releases No releases published Create a new release Packages No packages
+			published Publish your first package Contributors 2 @Z1xonu4a Z1xonu4a
+			Z1xonu4a @xyzrxltxhh xyzrxltxhh Nikita Languages JavaScript 89.9%
+			TypeScript 8.6% HTML 1.5% Footer © 2023 GitHub, Inc. Footer navigation
+			Terms Privacy Security Status Docs Contact GitHub Pricing API Training
+			Blog About
+		</Container>
+	)
+}
